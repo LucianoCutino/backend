103dced22a027dd3c6189a2461174fa31ffe1e20
Luciano Cutino <lucianocutino@gmail.com>
2022-08-01 12:07:00 -0300

    backend

---
+++ backend/routes/admin/login.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+res.render('admin/login',{
+    layout: 'admin/layout'
+    });
+});
+
+router.get('/logout', function (req, res, next) {
+req.session.destroy(); //destruir las variables de sesión (id y usuario)
+res.render('admin/login', {
+    layout: 'admin/layout'
+    });
+}); //cierro logout
+
+router.post('/', async (req, res, next) => {
+    try {
+        var usuario = req.body.usuario; //capturando la info// luciano en este caso
+        var password = req.body.password; // 1234
+
+        var data = await usuariosModel.getUserByUsernameAndPassword(usuario, password);
+    
+        if (data != undefined) {
+
+            req.session.id_usuario = data;
+            req.session.nombre = data.usuario;
+
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/login' , {
+                layout: 'admin/layout',
+                error: true
+            });
+        }
+    } catch (error) {
+        console.log (error);
+    } //cierro catch 
+}); //cierro router.post
+
+
+
+module.exports = router;
